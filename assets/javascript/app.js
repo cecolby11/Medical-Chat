23fddd6df94b08d3237a4c4dcbd0a56754e9a3b9
cecolby11 <cecolby11@users.noreply.github.com>
2017-03-15 19:11:25 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+//================
+// FIREBASE SETUP
+//================
+
+   // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBcxlMvEGf_-Ms_WzguEUmTmh3xCZWc9X4",
+    authDomain: "medical-chat.firebaseapp.com",
+    databaseURL: "https://medical-chat.firebaseio.com",
+    storageBucket: "medical-chat.appspot.com",
+    messagingSenderId: "1065425949070"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+//===========
+// APP STATE
+//=========== 
+
+var appState = {
+  phase: "initialize",
+  userInput: null,
+  emergencyLevel: null
+
+};
+
+//===================
+// EVENT MANAGEMENT
+//==================
+
+$('.chat-submit').on('click', function() {
+  event.preventDefault();
+  appState.userInput = $('.chat-input').val().trim();
+  // Fix this: 
+  appState.emergencyLevel = $('.chat-emergency-level').val();
+
+  console.log(userInput);
+  console.log(emergencyLevel);
+
+
+// ==================
+// READ FROM FIREBASE
+// ==================
+
+function getMessagesFromFirebase() {
+  database.ref().limitToLast(10).on('child_added'
+  }
+
+}
+
+//======================
+// SEND/SAVE TO FIREBASE
+//======================
+
+function storeMessageOnFirebase() {
+
+
+}
+
+
+});
+
+
+
+
+
+
+});
