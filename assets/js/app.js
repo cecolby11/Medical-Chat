a970fddcb8add191dabc32b71faf847bc17c75b3
cecolby11 <cecolby11@users.noreply.github.com>
2017-03-15 20:42:59 -0500

    added timestamp, starting authentication

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,15 +16,52 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
+  function authorizationSetup() {
+     firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          user.getToken().then(function(accessToken) {
+            document.getElementById('sign-in-status').textContent = 'Signed in';
+            document.getElementById('sign-in').textContent = 'Sign out';
+            document.getElementById('account-details').textContent = JSON.stringify({
+              displayName: displayName,
+              email: email,
+              emailVerified: emailVerified,
+              photoURL: photoURL,
+              uid: uid,
+              accessToken: accessToken,
+              providerData: providerData
+            }, null, '  ');
+          });
+        } else {
+          // User is signed out.
+          document.getElementById('sign-in-status').textContent = 'Signed out';
+          document.getElementById('sign-in').textContent = 'Sign in';
+          document.getElementById('account-details').textContent = 'null';
+        }
+      }, function(error) {
+        console.log(error);
+      });
+  }
+
 //===========
 // APP STATE
 //=========== 
 
-  var appState = {
-    phase: "initialize",
-    userName: "Caryn"
+var appState;
+function resetAppState() {
+  return {
+    phase: 'initialize',
+    userName: 'Caryn'
+  }
+}
 
-  };
 
 //===================
 // EVENT MANAGEMENT
@@ -35,9 +72,12 @@ $(document).ready(function() {
     var userInput = $('.chat-input').val().trim();
     // Fix this: 
     var emergencyLevel = $('.chat-emergency-level').val();
+    var timestamp = moment().format();
+    console.log(timestamp);
     var messageObject = {
       text: userInput,
-      name: appState.userName
+      name: appState.userName,
+      timestamp: timestamp
     };
 
     storeMessageOnFirebase(messageObject);
@@ -93,9 +133,16 @@ $(document).ready(function() {
 
 
 
-//=======================
+//=================
+// INITIALIZE APP
+//=================
+function initializeApp() {
+  appState = resetAppState();
+  authorizationSetup();
+  getMessagesFromFirebase(); 
+}
 
-  getMessagesFromFirebase();
+initializeApp();
 
 
 });
